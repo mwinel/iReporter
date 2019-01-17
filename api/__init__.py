cea877713c83b70ce32164d3a95941eec77df923
mwinel <mwinel@live.com>
2019-01-17 13:23:34 +0300

    chore(api-endpoints): Register blueprints

--- api/__init__.py
+++ api/__init__.py
@@ -46,11 +46,6 @@ app.errorhandler(404)(RequestError.not_found)
 app.errorhandler(405)(RequestError.method_not_allowed)
 
 # Register blueprints
-# from api.index import api as index_blueprint
 app.register_blueprint(index_blueprint, url_prefix='/api/v1')
-
-# from api.auth import api as auth_blueprint
 app.register_blueprint(auth_blueprint, url_prefix='/api/v1/auth')
-
-# from api.redflags import api as redflags_blueprint
 app.register_blueprint(redflags_blueprint, url_prefix='/api/v1')
