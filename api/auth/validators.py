139c12f4ffeb5e8e830f2cbf88aa3aa42c352534
mwinel <mwinel@live.com>
2019-01-22 21:58:47 +0300

    chore(data-persistence): Add user validations

---
+++ api/auth/validators.py
@@ -0,0 +1,45 @@
+"""
+user validations class
+"""
+import re
+
+
+class UserValidations:
+    """
+    A class used to represent user validations
+    """
+    def validate_user_email(self, email):
+        """
+        validates user email
+        returns: error message
+        """
+        if not email or email.isspace():
+            return "Email field cannot be left empty."
+        if not re.match(r"[^@.]+@[A-Za-z]+\.[a-z]+", email):
+            return "Enter a valid email address."
+
+    def validate_user_password(self, password):
+        """
+        validates user password
+        returns: error message
+        """
+        if not password or password.isspace():
+            return "Password field cannot be left empty."
+        if len(password) < 6:
+            return "Password too short, must be atleast 6 characters or more."
+
+    def validate_user_input(self, username, firstname, lastname, othernames, phone_number):
+        """
+        validates user input
+        returns: error message
+        """
+        if not username or username.isspace():
+            return "Username field cannot be left empty."
+        if not firstname or firstname.isspace():
+            return "Firstname field cannot be left empty."
+        if not lastname or lastname.isspace():
+            return "Lastname field cannot be left empty."
+        if not othernames or othernames.isspace():
+            return "Othernames field cannot be left empty."
+        if not phone_number or phone_number.isspace():
+            return "Phone number field cannot be left empty."
