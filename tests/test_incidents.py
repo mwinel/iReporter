1da5f1740083ddcc90a3c65f09a3842d9e9b4c1c
mwinel <mwinel@live.com>
2019-01-23 20:44:32 +0300

    feat(api-endpoints): Add fetch redflags

--- tests/test_incidents.py
+++ tests/test_incidents.py
@@ -45,7 +45,8 @@ class RedflagTestCase(BaseTestCase):
         )
         result = json.loads(rv.data.decode())
         self.assertTrue(rv.status_code, 400)
-        self.assertTrue(result["error"] == "Status field cannot be left empty.")
+        self.assertTrue(result["error"] ==
+                        "Status field cannot be left empty.")
 
     def test_create_redflag_with_missing_incident_type(self):
         """Test API cannot create incident with missing incident field."""
@@ -65,7 +66,8 @@ class RedflagTestCase(BaseTestCase):
         )
         result = json.loads(rv.data.decode())
         self.assertTrue(rv.status_code, 400)
-        self.assertTrue(result["error"] == "Incident Type field cannot be left empty.")
+        self.assertTrue(result["error"] ==
+                        "Incident Type field cannot be left empty.")
 
     def test_create_redflag_with_missing_location(self):
         """Test API cannot create redflag with missing location field."""
@@ -85,7 +87,8 @@ class RedflagTestCase(BaseTestCase):
         )
         result = json.loads(rv.data.decode())
         self.assertTrue(rv.status_code, 400)
-        self.assertTrue(result["error"] == "Location field cannot be left empty.")
+        self.assertTrue(result["error"] ==
+                        "Location field cannot be left empty.")
 
     def test_create_redflag_with_missing_image(self):
         """Test API cannot create redflag with missing image field."""
@@ -145,7 +148,8 @@ class RedflagTestCase(BaseTestCase):
         )
         result = json.loads(rv.data.decode())
         self.assertTrue(rv.status_code, 400)
-        self.assertTrue(result["error"] == "Comment field cannot be left empty.")
+        self.assertTrue(result["error"] ==
+                        "Comment field cannot be left empty.")
 
     def test_create_redflag_with_invalid_image(self):
         """Test API cannot create redflag with wrong image format."""
@@ -186,3 +190,22 @@ class RedflagTestCase(BaseTestCase):
         result = json.loads(rv.data.decode())
         self.assertTrue(rv.status_code, 400)
         self.assertTrue(result["error"] == "Invalid video format.")
+
+    def test_get_redflags(self):
+        """Test API can fetch all redflags."""
+
+        res = self.app.post(
+            'api/v2/auth/signup',
+            content_type='application/json',
+            data=json.dumps(self.user)
+        )
+
+        auth_token = json.loads(res.data.decode())
+        rv = self.app.get(
+            '/api/v2/red-flags',
+            headers={'Authorization': auth_token['access_token']},
+            content_type='application/json'
+        )
+        result = json.loads(rv.data.decode())
+        self.assertTrue(rv.status_code, 200)
+        self.assertTrue(result["message"] == "success")
