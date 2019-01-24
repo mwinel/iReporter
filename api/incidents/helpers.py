56dc222e4fc49ab78fa2cc330e2d4d4a6eb7b3ce
mwinel <mwinel@live.com>
2019-01-24 15:54:25 +0300

    feat(api-endpoints): Add get interventions

--- api/incidents/helpers.py
+++ api/incidents/helpers.py
@@ -8,8 +8,17 @@ db = DatabaseConnection()
 
 def get_redflags_by_redflag_type():
     """
-    returns a list of redflags by redflag type
+    returns a list of incidents by redflag type
     """
     incident_type = 'red-flag'
     redflags = db.get_all_by_argument('incidents', 'incident_type', incident_type)
     return redflags
+
+
+def get_interventions_by_intervention_type():
+    """
+    returns a list of incidents by interventions type
+    """
+    incident_type = 'intervention'
+    interventions = db.get_all_by_argument('incidents', 'incident_type', incident_type)
+    return interventions
